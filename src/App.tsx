0d7a1b3a49358db23e343d2b34f7966ead1ac2bd
sana-dev <55602404+sana-dev@users.noreply.github.com>
2024-04-30 14:09:06 +0200

    renaming files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.tsx';
-import Mood from './pages/auth/mood.tsx';
+import Mood from './pages/auth/Mood.tsx';
 import Movies from './pages/movies/movies';
 // import PlayPause from './components/Button/button.tsx';
-import Hero from './components/hero/hero.tsx';
+import Hero from './components/hero/Hero.tsx';
 import TvShow from './pages/tv-show/tv-show.tsx';
-import MyComponent from './pages/mood/mood.tsx';
+import MyComponent from './pages/mood/Mood.tsx';
 function App() {
   return (
     <div>
